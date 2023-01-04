0b3a091cbe33cae0ce5ea0c3d498b022e509fbcb
Mykola Dzoban <nick.dzoban@gmail.com>
2023-01-04 15:13:14 +0200

    start to work with js for project

---
+++ script.js
@@ -0,0 +1,7 @@
+const dom = {
+    new: document.getElementById('new'),
+    add: document.getElementById('add'),
+    tasks: document.getElementById('tasks')
+}
+
+// відслідковуємо клік по кнопці добавити задачу
